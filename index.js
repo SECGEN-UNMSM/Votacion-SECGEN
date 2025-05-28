ab2dbfe275ce0463559ecf39f661330a8b0dbb72
Vilberto <vilberto.patricio@unmsm.edu.pe>
2025-05-27 19:18:25 -0500

    Agregar dependencia para los cors

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 3001;
 
+const corsOptions = {
+  origin: ["http://localhost:3000/", "http://localhost:3001/"],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}; 
+  
+  
 app.use(express.json());
 
 const votoRoutes = require('./server/routes/votos.routes');
 const asambleistaRoutes = require('./server/routes/asambleista.routes');
 const candidatoRoutes = require('./server/routes/candidato.routes');
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use('/api', asambleistaRoutes);
 app.use('/api', candidatoRoutes);
 app.use('/api', votoRoutes);
+
 
 // Ruta base
 app.get('/', (req, res) => {
