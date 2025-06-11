e689c4b6d0f747b1ec8d30f1dfcc3b78f66aece6
antidv <miguelveraalva@gmail.com>
2025-06-11 14:49:38 -0500

    Modificar CORS del backend

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3001;
 
 const corsOptions = {
-  origin: ["http://localhost:3000/", "http://localhost:3001/"],
+  origin: ["http://localhost:3000/", "http://localhost:3001/", ""],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -19,7 +19,20 @@ const candidatoRoutes = require('./server/routes/candidato.routes');
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || origin === 'null') {
+        // Permitir si el origen es null (como desde Tauri)
+        callback(null, true);
+      } else {
+        // También puedes permitir tu frontend online, si aplica
+        const allowedOrigins = ["http://localhost:3000/"];
+        if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      }
+    },
     credentials: true,
   })
 );
