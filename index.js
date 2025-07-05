750079970518ec9f4d00cf95c0a744700fb6d27f
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2025-07-05 12:54:48 -0500

    Seguridad back

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
+
+app.disable('x-powered-by');
+
 const port = 3001;
 
 const corsOptions = {
-  origin: ["http://localhost:3000/", "http://localhost:3001/", ""],
+  origin: [
+    "tauri:://localhost",
+    "https://tauri.localhost/",
+    "http://tauri.localhost/",
+    "com.sistemavotacion.dev",
+    "tauri.localhost",
+    "http://localhost:3000",
+    "http://localhost:3001",
+  ],
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-}; 
-  
-  
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const votoRoutes = require('./server/routes/votos.routes');
 const asambleistaRoutes = require('./server/routes/asambleista.routes');
 const candidatoRoutes = require('./server/routes/candidato.routes');
 
-app.use(
-  cors({
-    origin: [
-      "tauri:://localhost",
-      "https://tauri.localhost/",
-      "http://tauri.localhost/",
-      "com.sistemavotacion.dev",
-      "tauri.localhost",
-      "http://localhost:3000",
-    ]
-  })
-);
 app.use('/api', asambleistaRoutes);
 app.use('/api', candidatoRoutes);
 app.use('/api', votoRoutes);
 
-
-// Ruta base
 app.get('/', (req, res) => {
   res.send('¡Backend funcionando!');
 });
 
-app.listen(port, () => {
-  console.log(`Servidor backend escuchando en http://localhost:${port}`);
-});
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Servidor backend escuchando en http://localhost:${port}`);
+  });
+}
