82661215cbafb98ff39521e1fe92679390be3391
antidv <miguelveraalva@gmail.com>
2025-09-25 11:20:43 -0500

    url

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:3001/api";
+export const baseURL = "https://sistemavotacion-secgen.onrender.com/api";
 /*
 https://sistemavotacion-secgen.onrender.com/api
 http://localhost:3001/api
